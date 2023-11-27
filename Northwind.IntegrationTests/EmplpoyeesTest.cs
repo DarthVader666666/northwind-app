ed1172d0d7deb87d88d4e9221a6b26487eb72dec
Vadzim Rumiantsau <rumyancer@gmail.com>
2023-11-27 19:43:53 +0300

    Created Integration Test project

---
+++ Northwind.IntegrationTests/EmplpoyeesTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NorthwindApiApp;
+using Xunit;
+
+namespace Northwind.IntegrationTests
+{
+    public class EmplpoyeesTest
+    {
+        [Fact]
+        public async Task Get_Employees()
+        {
+            using var application = new WebApplicationFactory<Startup>();
+            using var client = application.CreateClient();
+
+            var response = await client.GetAsync("/api/employees/total");
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            var amountOfEmployees = await response.Content.ReadAsStringAsync();
+            Assert.True(int.TryParse(amountOfEmployees, out _));
+            Assert.True(int.Parse(amountOfEmployees) >= 0);
+        }
+    }
+}
